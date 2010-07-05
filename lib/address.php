d354f905a97d90c4a14cd7b723540bbc0316487f
Mitsukuni Sato <key@Mitsukuni-Sato-no-MacBook-Pro.local>
2010-07-05 11:26:48 +0900

    $BETF;I\8)$HM9JXHV9f$rDI2C(B

--- lib/address.php
+++ lib/address.php
@@ -22,6 +22,9 @@ class Address extends Faker
 	private static $_uk_countries = array('England', 'Scotland','Wales', 'Northern Ireland');
 	private static $_uk_postcode_formats = array( '??## #??', '??# #??' );
 	private static $_street_name_formats = array('first_name','surname');
+
+	private static $_jp_prefectures = array('北海道','青森県','岩手県','宮城県','秋田県','山形県','福島県','茨城県','栃木県','群馬県','埼玉県','千葉県','東京都','神奈川県','新潟県','富山県','石川県','福井県','山梨県','長野県','岐阜県','静岡県','愛知県','三重県','滋賀県','京都府','大阪府','兵庫県','奈良県','和歌山県','鳥取県','島根県','岡山県','広島県','山口県','徳島県','香川県','愛媛県','高知県','福岡県','佐賀県','長崎県','熊本県','大分県','宮崎県','鹿児島県','沖縄県');
+	private static $_jp_postcode_formats = array( '###-####' );
 	
 	public function __construct()
 	{
@@ -106,7 +109,18 @@ class Address extends Faker
 		$result = parent::numerify( $a );
 		return $result;
 	}
-		
+	
+	###### Japan Only ########
+	public function jp_prefecture()
+	{
+		return parent::random( self::$_jp_prefectures );
+	}
+	public function jp_post_code()
+	{
+		$a = parent::random( self::$_jp_postcode_formats );
+		$result = parent::numerify( $a );
+		return $result;
+	}
 }
 
 ?>
